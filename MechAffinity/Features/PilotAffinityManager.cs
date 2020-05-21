6ab202b9471e35d5b04996a03f68e1dda4f52a50
Bill Torode <wmtorode@gmail.com>
2020-05-20 22:59:49 -0400

    start work on affinity management

---
+++ MechAffinity/Features/PilotAffinityManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MechAffinity.Data;
+using BattleTech;
+
+namespace MechAffinity
+{
+    class PilotAffinityManager
+    {
+        private static readonly string MA_Deployment_Stat = "MA_DeployStat_";
+        private static PilotAffinityManager instance;
+        private StatCollection companyStats;
+
+        public static PilotAffinityManager Instance
+        {
+            get
+            {
+                if (instance == null) instance = new PilotAffinityManager();
+                return instance;
+            }
+        }
+
+        public void initialize()
+        {
+
+        }
+
+        public void setCompanyStats(StatCollection stats)
+        {
+            companyStats = stats;
+        }
+    }
+}
--- MechAffinity/Main.cs
+++ MechAffinity/Main.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using MechAffinity.Data;
+using Harmony;
+using System.Reflection;
 
 namespace MechAffinity
 {
     public class Main
     {
+        internal static Logger modLog;
+        internal static Settings settings;
+        internal static string modDir;
+
+        public static void Init(string modDirectory, string settingsJSON)
+        {
+
+            modDir = modDirectory;
+            modLog = new Logger(modDir, "MechAffinity", true);
+
+            try
+            {
+                using (StreamReader reader = new StreamReader($"{modDir}/settings.json"))
+                {
+                    string jdata = reader.ReadToEnd();
+                    settings = JsonConvert.DeserializeObject<Settings>(jdata);
+                }
+                PilotAffinityManager.Instance.initialize();
+
+            }
+
+            catch (Exception ex)
+            {
+                modLog.LogException(ex);
+            }
+
+            var harmony = HarmonyInstance.Create("ca.jwolf.MechAffinity");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        }
     }
 }
