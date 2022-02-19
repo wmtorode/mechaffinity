cee9e46c0d9364e6a21daf07bb2ce71e1228a6e7
Bill Torode <wmtorode@gmail.com>
2022-02-18 21:15:51 -0500

    super breaching shot?

---
+++ MechAffinity/Patches/AttackSequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BattleTech;
+using BattleTech.UI;
+using BattleTech.StringInterpolation;
+using BattleTech.UI.TMProWrapper;
+using Localize;
+using Harmony;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using MechAffinity;
+
+namespace MechAffinity.Patches
+{
+    [HarmonyPatch(typeof(AttackDirector.AttackSequence), "get_IsBreachingShot")]
+    class AttackSequence_IsBreachingShot
+    {
+        private const string superBreachingShot = "SuperPrecisionShot";
+        public static void Postfix(AttackDirector.AttackSequence __instance, ref bool __result)
+        {
+            if (!__result)
+            {
+                __result = __instance.attacker.StatCollection.GetValue<bool>(superBreachingShot) && __instance.allSelectedWeapons[0].Type != WeaponType.Melee;
+            }
+        }
+    }
+}
