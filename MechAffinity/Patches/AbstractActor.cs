f56543a8c5132170fe2adcddd93404e1f5e4eb1f
Bill Torode <wmtorode@gmail.com>
2022-02-19 20:15:08 -0500

    more patches!

--- MechAffinity/Patches/AbstractActor.cs
+++ MechAffinity/Patches/AbstractActor.cs
@@ -20,4 +20,28 @@ namespace MechAffinity.Patches
             __instance.StatCollection.AddStatistic<bool>(AttackSequence_IsBreachingShot.superBreachingShot, false);
         }
     }
+
+    [HarmonyPatch(typeof(AbstractActor), "get_HasBreachingShotAbility")]
+    class AbstractActor_HasBreachingShotAbility
+    {
+        public static void Postfix(AbstractActor __instance, ref bool __result)
+        {
+            if (!__result)
+            {
+                __result = __instance.StatCollection.GetValue<bool>(AttackSequence_IsBreachingShot.superBreachingShot);
+            }
+        }
+    }
+    
+    [HarmonyPatch(typeof(AbstractActor), "IsUsingBreachingShotAbility")]
+    class AbstractActor_IsUsingBreachingShotAbility
+    {
+        public static void Postfix(AbstractActor __instance, ref bool __result)
+        {
+            if (!__result)
+            {
+                __result = __instance.StatCollection.GetValue<bool>(AttackSequence_IsBreachingShot.superBreachingShot);
+            }
+        }
+    }
 }
